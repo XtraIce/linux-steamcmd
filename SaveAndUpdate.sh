bf87e0141d10dc490058c9ae5c82304f933e11cc
Riker Quintana <rikerq.great95@gmail.com>
2025-09-15 21:10:02 -0700

    Improve broadcast messages in SaveAndUpdate script for server shutdown notifications

--- SaveAndUpdate.sh
+++ SaveAndUpdate.sh
@@ -15,16 +15,16 @@ ARRCON="/usr/bin/ARRCON"
 #Check if Server is running
 if $(pgrep -f $TARGET_APP >/dev/null) ; then
 	#Broadcast that the server will be going down soon.
-	echo "Broadcast Server_will_be_shutdown_in_30_mins" | $ARRCON -S game_server && echo "Broadcast for_scheduled_maintanence_-Red" | ARRCON -S game_server
+	echo "${RCON_BROADCAST_CMD} Server_will_be_shutdown_in_30_mins" | $ARRCON -S game_server && echo "${RCON_BROADCAST_CMD} for_scheduled_maintanence_-Red" | $ARRCON -S game_server
 	if [ $? -eq 0 ]; then
 		sleep 20m
-		echo "Broadcast Server_will_be_shutdown_in_10_mins" |$ARRCON -S game_server && echo "Broadcast for_scheduled_maintanence" | ARRCON -S game_server
+		echo "${RCON_BROADCAST_CMD} Server_will_be_shutdown_in_10_mins" | $ARRCON -S game_server && echo "${RCON_BROADCAST_CMD} for_scheduled_maintanence" | $ARRCON -S game_server
 		sleep 9m 30s
-		echo "Broadcast SERVER_IS_SHUTTING_DOWN" | $ARRCON -S game_server
-		echo "Broadcast Est_restart_time_<_2MIN" | $ARRCON -S game_server
-		echo "Broadcast DRINK_SOME_WATER" | $ARRCON -S game_server
+		echo "${RCON_BROADCAST_CMD} SERVER_IS_SHUTTING_DOWN" | $ARRCON -S game_server
+		echo "${RCON_BROADCAST_CMD} Est_restart_time_<_2MIN" | $ARRCON -S game_server
+		echo "${RCON_BROADCAST_CMD} DRINK_SOME_WATER" | $ARRCON -S game_server
 		sleep 10
-		echo "Save" | $ARRCON -S game_server
+		echo "${RCON_BROADCAST_CMD} Save" | $ARRCON -S game_server
 	else
 		echo "$RCON Failed. Shutting down now."
 	fi
