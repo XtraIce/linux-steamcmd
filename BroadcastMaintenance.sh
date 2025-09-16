24312f65701c3cee31b5e157b7e555704e4e3f6a
Riker Quintana <rikerq.great95@gmail.com>
2025-09-15 19:22:28 -0700

    Add BroadcastMaintenance script and update service to include maintenance broadcasts

---
+++ BroadcastMaintenance.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Broadcast maintenance messages to the game server via ARRCON
+ARRCON="/usr/bin/ARRCON"
+TARGET_APP=$(basename "${START_SCRIPT:-${GAME_EXECUTABLE}}")
+
+if $BROADCAST_MAINTENANCE; then
+    # Broadcast sequence
+    if $(pgrep -f $TARGET_APP >/dev/null) ; then
+        echo "Broadcast Server_will_be_shutdown_in_30_mins" | $ARRCON -S game_server && \
+        echo "Broadcast for_scheduled_maintanence_-Red" | $ARRCON -S game_server
+        if [ $? -eq 0 ]; then
+            sleep 20m
+            echo "Broadcast Server_will_be_shutdown_in_10_mins" | $ARRCON -S game_server && \
+            echo "Broadcast for_scheduled_maintanence" | $ARRCON -S game_server
+            sleep 9m 30s
+            echo "Broadcast SERVER_IS_SHUTTING_DOWN" | $ARRCON -S game_server
+            echo "Broadcast Est_restart_time_<_2MIN" | $ARRCON -S game_server
+            echo "Broadcast DRINK_SOME_WATER" | $ARRCON -S game_server
+            sleep 10
+            echo "Save" | $ARRCON -S game_server
+        else
+            echo "$RCON Failed. Shutting down now."
+        fi
+    fi
+else
+    echo "Broadcast maintenance messages disabled. Proceeding without broadcast."
+fi
