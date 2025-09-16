c02b3a886831f3749bf04fb6e7355a6606ff75f7
Riker Quintana <rikerq.great95@gmail.com>
2025-09-15 21:19:27 -0700

    Refactor broadcast messages in BroadcastMaintenance script to use RCON_BROADCAST_CMD for consistency

--- BroadcastMaintenance.sh
+++ BroadcastMaintenance.sh
@@ -6,18 +6,18 @@ TARGET_APP=$(basename "${START_SCRIPT:-${GAME_EXECUTABLE}}")
 if $BROADCAST_MAINTENANCE; then
     # Broadcast sequence
     if $(pgrep -f $TARGET_APP >/dev/null) ; then
-        echo "Broadcast Server_will_be_shutdown_in_30_mins" | $ARRCON -S game_server && \
-        echo "Broadcast for_scheduled_maintanence_-Red" | $ARRCON -S game_server
+        echo "${RCON_BROADCAST_CMD} Server_will_be_shutdown_in_30_mins" | $ARRCON -S game_server && \
+        echo "${RCON_BROADCAST_CMD} for_scheduled_maintanence_-Red" | $ARRCON -S game_server
         if [ $? -eq 0 ]; then
             sleep 20m
-            echo "Broadcast Server_will_be_shutdown_in_10_mins" | $ARRCON -S game_server && \
-            echo "Broadcast for_scheduled_maintanence" | $ARRCON -S game_server
+            echo "${RCON_BROADCAST_CMD} Server_will_be_shutdown_in_10_mins" | $ARRCON -S game_server && \
+            echo "${RCON_BROADCAST_CMD} for_scheduled_maintanence" | $ARRCON -S game_server
             sleep 9m 30s
-            echo "Broadcast SERVER_IS_SHUTTING_DOWN" | $ARRCON -S game_server
-            echo "Broadcast Est_restart_time_<_2MIN" | $ARRCON -S game_server
-            echo "Broadcast DRINK_SOME_WATER" | $ARRCON -S game_server
+            echo "${RCON_BROADCAST_CMD} SERVER_IS_SHUTTING_DOWN" | $ARRCON -S game_server
+            echo "${RCON_BROADCAST_CMD} Est_restart_time_<_2MIN" | $ARRCON -S game_server
+            echo "${RCON_BROADCAST_CMD} DRINK_SOME_WATER" | $ARRCON -S game_server
             sleep 10
-            echo "Save" | $ARRCON -S game_server
+            echo "${RCON_BROADCAST_CMD} Save" | $ARRCON -S game_server
         else
             echo "$RCON Failed. Shutting down now."
         fi
